c72fb1ac0a1f9c3b180c496b2cf53d698bf1a2b7
Jatin75B <84146012+Jatin75B@users.noreply.github.com>
2023-08-27 00:02:17 +0530

    Add files via upload

---
+++ Pascal_triangle.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    // speed=O(N^2)
+    // space=O(1)   only ans is stored
+public:
+    vector<int> row(int n)
+    {
+        vector<int> ans;
+        int a = 1;
+        ans.push_back(1);
+        for (int i = 1; i < n; i++)
+        {
+            a *= n - i;
+            a /= i;
+            ans.push_back(a);
+        }
+        return ans;
+    }
+
+    vector<vector<int>> generate(int N)
+    {
+        vector<vector<int>> ans;
+        for (int i = 1; i <= N; i++)
+            ans.push_back(row(i));
+
+        return ans;
+    }
+};
